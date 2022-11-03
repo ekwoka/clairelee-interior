caf1a11c33c48bc77844567a9b94b79da893839d
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-11-03 12:30:24 +0400

    :recycle: Changes how Cloudinary is Used

--- functions/image.ts
+++ functions/image.ts
@@ -1,22 +1,15 @@
 import { Context } from 'https://edge.netlify.com';
 
-export default async (req: Request, ctx: Context) => {
+export default (req: Request, ctx: Context) => {
   const isDev = Deno.env.get('NETLIFY_DEV') === 'true';
   const url = new URL(req.url);
   const filename = url.pathname.replace('/image/', '');
   if (isDev) return ctx.rewrite(`/images/${filename}`);
   const width = url.searchParams.get('w') ?? '200';
-  const response = await fetch(
+  return Response.redirect(
     makeCloudURL(`${ctx.site.url}/images/${filename}`, width),
-    {
-      headers: req.headers,
-    }
+    302
   );
-  return new Response(response.body, {
-    headers: response.headers,
-    status: response.status,
-    statusText: response.statusText,
-  });
 };
 
 const makeCloudURL = (path: string, width: string) =>
